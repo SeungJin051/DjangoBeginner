505f34166d93f6f09ad5679a6f7e81711113d7db
SeungJin051 <83889135+SeungJin051@users.noreply.github.com>
2023-11-27 15:33:29 +0900

    Django URLs와 Views에 대해 알아보기

--- polls/views.py
+++ polls/views.py
@@ -2,4 +2,14 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, MY APP")
+    return HttpResponse("Hello, polls")
+
+def detail(request, question_id):
+    return HttpResponse("질문 %s." % question_id)
+
+def results(request, question_id):
+    response =  "현재 질문의 결과는 %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("voting 질문 %s." % question_id)
