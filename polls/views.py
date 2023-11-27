2984625d698f3d0ed632437d8f7b9a05991d111e
SeungJin051 <83889135+SeungJin051@users.noreply.github.com>
2023-11-27 16:02:13 +0900

    Django Reverse(리버스) URL에 대해 알아보기

--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,12 @@
 from django.http import HttpResponse, HttpResponseRedirect
-
+from django.urls import reverse
 
 def index(request):
-    return HttpResponseRedirect('1')
+    # return HttpResponseRedirect('1')
+    return HttpResponseRedirect(
+        # reverse('detail', args=[1])
+        reverse('detail', kwargs={'question_id' : 1})
+    )
 
 def detail(request, question_id):
     return HttpResponse("질문 %s." % question_id)
