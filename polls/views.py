59fa43aec7d37116fc4dc038d51953806724166f
SeungJin051 <83889135+SeungJin051@users.noreply.github.com>
2023-12-05 17:39:51 +0900

    Django Template(템플릿) Commands(명령어)에 대해 알아보기
    웹 프레임워크이기 때문에 Django는 HTML을 동적으로 생성하는 편리한 방법이 필요합니다. 가장 일반적인 접근 방식은 템플릿을 사용합니다. 템플릿에는 원하는 HTML 출력의 정적 부분과 동적 콘텐츠가 삽입되는 방법을 설명하는 일부 특수 구문이 포함되어 있습니다.

--- polls/views.py
+++ polls/views.py
@@ -3,7 +3,15 @@ from django.urls import reverse
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'polls/main.html')
+    ctx = {
+        "greetings" : "Hello there",
+        "location" : {
+            "city" :"Busan",
+            "country" : "South Korea"
+        },
+        "languages" : ["Korean", "English"]
+    }
+    return render(request, 'polls/main.html', context=ctx)
 
 def detail(request, question_id):
     return HttpResponse("질문 %s." % question_id)
